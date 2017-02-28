ecbab8b3712d9d692b8204c8c93e6ecd04e2827e
Reuben Sinha <knotseaborg@gmail.com>
2017-02-28 21:04:03 +0530

    (Trial to understand GitHub)Uploaded a rough design for: 1. Contact 2. Welcome 3. About

---
+++ resources/views/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('partials._header')
+  </head>
+  <body>
+    @include('partials._nav')
+    <div class="container">
+    @yield('content')
+    </div>
+    @include('partials._footer')
+  </body>
+</html>
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,9 @@
+<hr>
+<div class="row">
+    <div class="col-md-10 col-md-offset-1 text-center">
+        <div>
+            Copyrights @MarketerHub
+        </div>
+    </div>
+</div>
+@include('partials._script')
